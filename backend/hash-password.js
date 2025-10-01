e6291c8b695b17508a31f0d56165acafb66cfaab
John Pretz <jjp6846@psu.edu>
2025-10-01 13:05:46 -0400

    Remove all files from repository

--- backend/hash-password.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const password = 'pass123'; // the password you want to hash
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds, (err, hash) => {
-  if (err) throw err;
-  console.log('Bcrypt hash:', hash);
-});
